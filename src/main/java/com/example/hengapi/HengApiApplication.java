ffe45026fc9c647d4b74c0976ce2732b2116c521
iamnamheng@gmail.com <iamnamheng@gmail.com>
2023-03-08 10:26:04 +0700

    first commit

---
+++ src/main/java/com/example/hengapi/HengApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hengapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HengApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HengApiApplication.class, args);
+    }
+
+}
